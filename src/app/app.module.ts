0b5994e9012121f99754f38e8bf6985de26e2417
Alejandro <alerodriguez692@gmail.com>
2023-07-28 11:41:54 -0300

    Paso a angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    EquipoComponent,
+    ContactoComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
